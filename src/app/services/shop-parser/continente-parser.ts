b1e6e6804b115cadb379588780eb9a40e4daec24
Freitolini <tfreitas.me@gmail.com>
2024-10-28 20:00:35 +0000

    Refactor LidlParser and ContinenteParser classes for improved code readability and parsing efficiency

--- src/app/services/shop-parser/continente-parser.ts
+++ src/app/services/shop-parser/continente-parser.ts
@@ -4,7 +4,16 @@ import { ShopParser } from "./shop-parser";
 
 export class ContinenteParser extends ShopParser{
     capitalLetter:RegExp = new RegExp('/^[A-Z]/', 'g');
+    descontoRegex:RegExp = new RegExp('^(POUPANCA|PDUPANCA) \\d+(\\.\\d+)?$', 'g');
+    descontoDirectRegex:RegExp = new RegExp('^(DESCONTO DIRETO) \\d+(\\.\\d+)?$', 'g');
+    quantityRegex:RegExp = new RegExp('^\\d+(\\.\\d+)?\\s*X\\s*\\d+(\\.\\d+)?', 'gi');
+    
 
+    protected override multiLineCase(line:string, lines :string[]) {
+        this.checkforDiscount(lines);
+        this.addProdcut();
+        this.state = ParserState.ProductSearch;
+    }
 
     protected override dateSearchCase(line:string, lines :string[]): void {
         if (line.includes("Nro:")) {
@@ -27,10 +36,42 @@ export class ContinenteParser extends ShopParser{
             return
         }
         let splits:string[] = line.trim().split(" ");
-        this.price = parseFloat(splits[splits.length-1].replace(",","."));
         this.description = splits.slice(1,splits.length-1).join(" ");
+        if (this.checkforMultiple(lines) || this.checkforDiscount(lines)){
+            return
+        }
+        this.price = parseFloat(splits[splits.length-1].replace(",","."));
         this.addProdcut();
     }
+
+    checkforDiscount(lines :string[]): boolean {
+        let line = lines[this.idx+1].replace(",",".");
+        let result = this.descontoRegex.exec(line) && this.descontoDirectRegex.exec(line);
+        let discount = NaN;
+        if (result){
+            discount = parseFloat(result[1]);
+        }
+        if (!isNaN(discount)){
+            let nextLine = lines[this.idx+1].trim().split(" ");
+            let discount = parseFloat(nextLine[nextLine.length-1].replace(",","."));
+            this.price = this.price - discount;
+            this.state = ParserState.MultiLine; 
+            return true;
+        }
+        return false;
+    }
+
+    private checkforMultiple(lines:string[]):boolean{
+        if (this.quantityRegex.exec(lines[this.idx+1].trim().replace(",","."))){
+            this.description += " " + lines[this.idx+1].trim();
+            let nextLine = lines[this.idx+1].trim().split(" ");
+            this.price = parseFloat(nextLine[nextLine.length-1].replace(",","."));
+            this.state = ParserState.MultiLine; 
+            return true;
+        }
+        return false;
+    }
+
     override getShopName(): string {
         return "Continente";    
     }
--- src/app/services/shop-parser/lidl-parser.ts
+++ src/app/services/shop-parser/lidl-parser.ts
@@ -5,9 +5,9 @@ import { ShopParser } from "./shop-parser";
 export class LidlParser extends ShopParser{
    
     protected productRegex = new RegExp('\\d+(\\.\\d+)? [A-Z]', 'g');
-    protected quantityKgRegex = new RegExp('\\d+(\\.\\d+)?kg x \\d+(\\.\\d+)?EUR/kg', 'g');
-    protected quantitygRegex = new RegExp('\\d+(\\.\\d+)?g x \\d+(\\.\\d+)?EUR/g', 'g');
-    protected quantityRegex = new RegExp('^\\d+(\\.\\d+)?x\\d+(\\.\\d+)?$', 'g');
+    protected quantityKgRegex = new RegExp('\\d+(\\.\\d+)? kg (x|X) \\d+(\\.\\d+)? EUR/kg', 'g');
+    protected quantitygRegex = new RegExp('\\d+(\\.\\d+)? g (x|X) \\d+(\\.\\d+)? EUR/g', 'g');
+    protected quantityRegex = new RegExp('\\d+(\\.\\d+)?\\s*x\\s*\\d+(\\.\\d+)?', 'g');
 
     protected override dateSearchCase(line:string, lines :string[]): void {
         if (line.includes("Original Data")) {
@@ -35,26 +35,32 @@ export class LidlParser extends ShopParser{
             return;
         }
         let splits:string[] = line.trim().split(" ");
-        this.price = parseFloat(splits[splits.length-2].replace(",","."));
+        this.price = parseFloat(splits[splits.length-2].replace(",",".").replaceAll("l","1"));
         this.description = splits.slice(0,splits.length-2).join(" ");
         this.checkForQuantaty(lines);            
         this.checkforDiscount(lines);    
         this.addProdcut();
     }
     checkForQuantaty(lines :string[]) {
-        let line = lines[this.idx+1].trim();
+        let line = lines[this.idx+1].trim().replaceAll(",",".");
         if(line.match(this.quantityKgRegex) || line.match(this.quantitygRegex) || line.match(this.quantityRegex)){
             this.description += " " + line;
+            this.idx++;
         }
     }
 
     checkforDiscount(lines :string[]) {
-        let probableDiscount = lines[this.idx+1];
-        if (probableDiscount.includes("Desconto Lidl Plus")) {
+        let currentIdx = this.idx+1;
+        let probableDiscount = lines[currentIdx];
+        while((probableDiscount.includes("Desconto") || probableDiscount.includes("DESCONTO"))){
             let splits = probableDiscount.split(" ");
-            let discount = parseFloat(splits[splits.length-1].replace(",",".").trim().substring(1));
-            this.price -= discount;            
-            this.idx++;
+            let discount = parseFloat(splits[splits.length-1].replace(",",".").trim().substring(1).replaceAll("l","1"));
+            this.price -= discount;
+            currentIdx++;
+            probableDiscount = lines[currentIdx];
+        }
+        if (currentIdx != this.idx+1) {
+            this.idx = currentIdx-1;  
         }
     }
 
